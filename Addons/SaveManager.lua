0a47cc88da5af7a0d95304604b190d6007273ea5
shakar <136544760+shakar60@users.noreply.github.com>
2025-04-23 18:43:23 +0300

    Update SaveManager.lua

--- Addons/SaveManager.lua
+++ Addons/SaveManager.lua
@@ -32,7 +32,7 @@ if typeof(copyfunction) == "function" then
 end
 
 local SaveManager = {} do
-    SaveManager.Folder = "RickWare"
+    SaveManager.Folder = "RickWareSettings"
     SaveManager.SubFolder = ""
     SaveManager.Ignore = {}
     SaveManager.Library = nil

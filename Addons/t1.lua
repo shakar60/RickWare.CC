79e18fe4d872b36562b2d98b0c54aa48609bcf53
shakar <136544760+shakar60@users.noreply.github.com>
2025-07-03 01:54:35 +0300

    Update t1.lua

--- Addons/t1.lua
+++ Addons/t1.lua
@@ -251,7 +251,7 @@ local ThemeManager = {} do
         groupbox:AddDropdown("FontFace", {
             Text = "Font Face",
             Default = "Code",
-            Values = {"BuilderSans", "Code", "Fantasy", "Gotham", "Jura", "Roboto", "RobotoMono", "SourceSans"}
+            Values = {"Arimo","Arcade","BuilderSans","Code","Creepster","Fantasy","Gotham","Jura","Oswald","PermanentMarker","Roboto","RobotoMono","SourceSans","SourceSansBold"}
         })
 
         

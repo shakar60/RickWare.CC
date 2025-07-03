9e68f67791a61fae0b2a28767f3c2131d2a34dbc
shakar <136544760+shakar60@users.noreply.github.com>
2025-07-03 03:55:09 +0300

    Update SaveManager.lua

--- Addons/SaveManager.lua
+++ Addons/SaveManager.lua
@@ -33,7 +33,7 @@ if typeof(copyfunction) == "function" then
 end
 
 local SaveManager = {} do
-    SaveManager.Folder = "ObsidianLibSettings"
+    SaveManager.Folder = "RickWare"
     SaveManager.SubFolder = ""
     SaveManager.Ignore = {}
     SaveManager.Library = nil
